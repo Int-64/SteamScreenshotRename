8ce0a107f6898fc8c4db7e093f947ee30fd50c7a
발광목성인 <jupiter1390@naver.com>
2024-11-20 22:47:33 +0900

    Update ssr.py
    Caching system for efficiency
    Prints progressed time

--- ssr.py
+++ ssr.py
@@ -1,25 +1,38 @@
 import os
 import yaml
+import time
+import datetime
 from steam_web_api import Steam
-
-def nidname(gameid):
-    appdata = steam.apps.get_app_details(gameid)
-    gamename = appdata.get(gameid).get("data").get("name")
-    return gamename
-
-def id2code(filename):
-    text = filename.split('_')
-    id = nidname(text[0])
-    id = wsafetext(id)
-    result = id + "_" + text[1] + "_" + text[2]
-    return result
 
 def wsafetext(text):
     for i in range(len(illegal)):
         text = text.replace(illegal[i], "")
     return text
 
+def nidname(gameid):
+    appdata = steam.apps.get_app_details(gameid)
+    gamename = appdata.get(gameid).get("data").get("name")
+    return gamename
+
+ncache = {}
+
+def cachename(gameid):
+    if ncache.get(gameid) != None:
+        gamename = ncache.get(gameid)
+    else:
+        gamename = nidname(gameid)
+        ncache.update({gameid: nidname(gameid)})
+    gamename = wsafetext(gamename)
+    return gamename
+
+def id2code(filename):
+    text = filename.split('_')
+    id = cachename(text[0])
+    result = id + "_" + text[1] + "_" + text[2]
+    return result
+
 def rename_files(path):
+    tstart = int(time.time())
     count = 0   
     for filename in os.listdir(path):
         filepath = os.path.join(path, filename)
@@ -31,7 +44,9 @@ def rename_files(path):
             continue
     if count == 0:
         count = "No"
-    print(f"{count} file names changed!")
+    tspent = int(time.time()) - tstart
+    tformat = str(datetime.timedelta(seconds = tspent))
+    print(f"{count} file names changed! Time Spent: {tformat}")
 
 with open("config.yml", "r") as file:
     config_var = yaml.safe_load(file)
